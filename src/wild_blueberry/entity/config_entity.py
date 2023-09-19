a933494d7db8d399dcdc55c95a0a60b6e5766dcb
gbiamgaurav <gaurav.bhattacharya10@gmail.com>
2023-09-19 19:56:45 +0530

    data transformation completed

--- src/wild_blueberry/entity/config_entity.py
+++ src/wild_blueberry/entity/config_entity.py
@@ -18,4 +18,9 @@ class DataValidationConfig:
     unzip_data_dir: Path
     all_schema: dict
 
-    
+
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
+    preprocessor_path: Path 
